728391340a7523a56554697485a618e6163e8c61
dingbotsllc <98507443+dingbotsllc@users.noreply.github.com>
2022-02-01 23:13:43 -0800

    Update App.js

--- DingBots Minter/src/App.js
+++ DingBots Minter/src/App.js
@@ -247,7 +247,7 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
-              {data.totalSupply} / {CONFIG.MAX_SUPPLY}
+              {data.totalSupply} / {CONFIG.MAX_SUPPLY+1}
             </s.TextTitle>
             <s.TextDescription
               style={{
@@ -260,7 +260,7 @@ function App() {
               </StyledLink>
             </s.TextDescription>
             <s.SpacerSmall />
-            {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
+            {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY+1 ? (
               <>
                 <s.TextTitle
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
